9f864d792b18306eba939bb7ca69efbdab55c381
celebrater <tkgm45nbchk@gmail.com>
2022-01-24 22:22:01 +0900

    fixed bugs

--- bbs/Comment.php
+++ bbs/Comment.php
@@ -5,6 +5,7 @@ class Comment
     public $id;
     public $reply; // ID 何番への返信か
     public $date; // 2022-01-24_00:00:00
+    public $date_string; // 2022-01-24_00:00:00
     public $date_unix; // unix time stamp
     public $sender; // 投稿者名
     public $title;
@@ -19,39 +20,46 @@ class Comment
 //        2|1|2007/05/25(Fri) 04:01:07|名も無き投稿者|無題|wf9NFq8ibtbks|https://google.com/|hoge123@google.com|192.168.1.100|60|0
 //        $id|$reply|$date|$date_unix|$sender|$title|$text|$unique|$hp|$mail|$ip|0（改行コードによるバグ対策）
         $list_data = explode("|", $line);
-        if(file_exists($list_data[0] . ".txt")){
-            $this->id = (int)$list_data[0];
-            $this->reply = (int)$list_data[1];
-            $this->date = $list_data[2];
-            $this->date_unix = $list_data[3];
-            $this->sender = $list_data[4];
-            $this->title = $list_data[5];
-            $this->unique = $list_data[6];
-            $this->hp = $list_data[7];
-            $this->mail = $list_data[8];
-            $this->ip = $list_data[9];
-//            $this->text = file($list_data[0] . ".txt");
-        } else {
-            $this->id = null;
-            $this->reply = null;
-            $this->date = null;
-            $this->date_unix = null;
-            $this->sender = null;
-            $this->title = $list_data[0] . ".txt が存在しないか、読み込めません。";
-//            $this->text = [$list_data[0] . ".txt が存在しないか、読み込めません。"];
-            $this->unique = null;
-            $this->hp = null;
-            $this->mail = null;
-            $this->ip = null;
-        }
+        $this->id = (int)$list_data[0];
+        $this->reply = (int)$list_data[1];
+        $this->date = $list_data[2];
+        $this->date_string = $list_data[2];
+        $this->date_unix = $list_data[3];
+        $this->sender = $list_data[4] === "" ? "名も無き投稿者" : $list_data[4];
+//            $this->title = $list_data[5];
+        $this->title = $list_data[5] === "" ? "無題" : $list_data[5];
+        $this->unique = $list_data[6] === "" ? "----------" : $list_data[6];
+        $this->hp = $list_data[7];
+        $this->mail = $list_data[8];
+        $this->ip = $list_data[9];
+        $this->get_text();
+//        if(file_exists($list_data[0] . ".txt")){
+////            $this->text = file($list_data[0] . ".txt");
+//        } else {
+//            $this->id = null;
+//            $this->reply = null;
+//            $this->date = null;
+//            $this->date_unix = null;
+//            $this->sender = null;
+//            $this->title = $list_data[0] . ".txt が存在しないか、読み込めません。";
+////            $this->text = [$list_data[0] . ".txt が存在しないか、読み込めません。"];
+//            $this->unique = null;
+//            $this->hp = null;
+//            $this->mail = null;
+//            $this->ip = null;
+
     }
 
     function get_text(){
-        $path = "comments/" . $this->id . ".txt";
+        $path = "bbs/comments/" . $this->id . ".txt";
         if(file_exists($path)){
             $this->text = file($path);
         } else {
             $this->text = [$path . "が存在しないか、読み込めません。"];
         }
     }
+
+//    function get_date_string($date){
+//
+//    }
 }
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once "bbs/Comment.php";
+
 $title = "PHP HP BBS";
 
 $list = file("bbs/list.txt");
@@ -8,7 +10,7 @@ $comments = get_comments($list);
 function get_comments($list){
     $array = [];
     foreach ($list as $line){
-        $temp = explode("|", $line);
+//        $temp = explode("|", $line);
         array_push($array, new Comment($line));
     }
     return $array;
@@ -24,9 +26,32 @@ function get_comments($list){
     <title><?php echo $title; ?></title>
 </head>
 <body>
+
     <div class="containter">
         <h1><?php echo $title; ?></h1>
         <p>Hello World.</p>
+        <?php foreach ($comments as $comment) : ?>
+            <div class="comment">
+                <hr>
+                <p>
+                    <?php echo $comment->id; ?>:
+                    <span class="name"><?php echo $comment->sender; ?></span>
+                    <?php echo $comment->date_string; ?>
+                    ID:<?php echo $comment->unique; ?>
+                    [返信]
+                </p>
+<!--                <h2>--><?php //echo $comment->title; ?><!--</h2>-->
+                <div class="text">
+                    <?php if($comment->reply !== 0 && $comment->reply !== "") : ?>
+                        <p>&gt;&gt; <?php echo $comment->reply; ?></p>
+                    <?php endif; ?>
+                    <?php foreach ($comment->text as $line) : ?>
+                        <p><?php echo $line; ?></p>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
+
         <div class="comment">
             <hr>
             <p>1: <span class="name">通りすがりの名無しさん</span> 2022/1/24 15:16:34 ID:yjLxrzJFriH9s [返信]</p>
