7bcfbeae4c1156afcb2b7574df81d8011afdfeea
celebrater <tkgm45nbchk@gmail.com>
2022-01-24 21:54:54 +0900

    create the base layout as html

--- bbs/Comment.php
+++ bbs/Comment.php
@@ -8,46 +8,50 @@ class Comment
     public $date_unix; // unix time stamp
     public $sender; // 投稿者名
     public $title;
-    public $text; // コメント内容
+    public $text = []; // コメント内容
+    public $unique; // 2ch とかのなりすまし対策のトリップ
     public $hp; // 投稿者のホームページの URL
     public $mail; // 投稿者のメールアドレス
-    public $unique; // 2ch とかのなりすまし対策のトリップ
     public $ip; // 投稿者の IP アドレス
-
 
 //     function __construct($id, $reply, $date, $title, $sender){
     function __construct($line){
-        // 2|1|2022-01-24_00:00:00|1255394956|名も無き者|テスト投稿|わははははははははは|yjLxrzJFriH9s|http://enin-world.sakura.ne.jp/|skmt.3b.kzm.knpch@gmail.com|192.168.1.100|0
-        // $id|$reply|$date|$date_unix|$sender|$title|$text|$unique|$hp|$mail|$ip|0（改行コードによるバグ対策）
+//        2|1|2007/05/25(Fri) 04:01:07|名も無き投稿者|無題|wf9NFq8ibtbks|https://google.com/|hoge123@google.com|192.168.1.100|60|0
+//        $id|$reply|$date|$date_unix|$sender|$title|$text|$unique|$hp|$mail|$ip|0（改行コードによるバグ対策）
         $list_data = explode("|", $line);
         if(file_exists($list_data[0] . ".txt")){
             $this->id = (int)$list_data[0];
             $this->reply = (int)$list_data[1];
             $this->date = $list_data[2];
-            $this->sender = $list_data[3];
-            $this->trip = $list_data[4];
+            $this->date_unix = $list_data[3];
+            $this->sender = $list_data[4];
             $this->title = $list_data[5];
-            $this->text = file($list_data[0] . ".txt");
+            $this->unique = $list_data[6];
+            $this->hp = $list_data[7];
+            $this->mail = $list_data[8];
+            $this->ip = $list_data[9];
+//            $this->text = file($list_data[0] . ".txt");
         } else {
             $this->id = null;
             $this->reply = null;
             $this->date = null;
+            $this->date_unix = null;
+            $this->sender = null;
             $this->title = $list_data[0] . ".txt が存在しないか、読み込めません。";
-            $this->text = [$list_data[0] . ".txt が存在しないか、読み込めません。"];
+//            $this->text = [$list_data[0] . ".txt が存在しないか、読み込めません。"];
+            $this->unique = null;
+            $this->hp = null;
+            $this->mail = null;
+            $this->ip = null;
         }
-//        if(file_exists($id . ".txt")){
-//            $this->id = $id;
-//            $this->reply = $reply;
-//            $this->date = $date;
-//            $this->title = $title;
-//            $this->sender = $sender;
-//            $this->text = file($id . ".txt");
-//        } else {
-//            $this->id = null;
-//            $this->reply = null;
-//            $this->date = null;
-//            $this->title = $id . ".txt が存在しないか、読み込めません。";
-//            $this->text = [$id . ".txt が存在しないか、読み込めません。"];
-//        }
+    }
+
+    function get_text(){
+        $path = "comments/" . $this->id . ".txt";
+        if(file_exists($path)){
+            $this->text = file($path);
+        } else {
+            $this->text = [$path . "が存在しないか、読み込めません。"];
+        }
     }
 }
--- index.php
+++ index.php
@@ -2,16 +2,40 @@
 
 $title = "PHP HP BBS";
 
+$list = file("bbs/list.txt");
+$comments = get_comments($list);
+
+function get_comments($list){
+    $array = [];
+    foreach ($list as $line){
+        $temp = explode("|", $line);
+        array_push($array, new Comment($line));
+    }
+    return $array;
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
     <title><?php echo $title; ?></title>
 </head>
 <body>
-    <h1><?php echo $title; ?></h1>
-    <p>Hello World.</p>
+    <div class="containter">
+        <h1><?php echo $title; ?></h1>
+        <p>Hello World.</p>
+        <div class="comment">
+            <hr>
+            <p>1: <span class="name">通りすがりの名無しさん</span> 2022/1/24 15:16:34 ID:yjLxrzJFriH9s [返信]</p>
+            <h2>タイトル</h2>
+            <div>
+                1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。1 番目の投稿です。
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
