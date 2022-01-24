4754c970bae6a0a0652d518054da5c5ff06a9e3b
celebrater <tkgm45nbchk@gmail.com>
2022-01-24 20:54:17 +0900

    output resorted list succeeded

--- bbs/test/index.php
+++ bbs/test/index.php
@@ -6,7 +6,7 @@ $list = "list.txt";
 //$txt = "";
 $log = file("test.log");
 
-//$i = 0;
+$i = 0;
 foreach ($log as $line){
     $replaced = str_replace("麻呂", "永人", $line);
     $replaced = str_replace("仲川", "富士見", $replaced);
@@ -34,15 +34,17 @@ foreach ($log as $line){
         "https://google.com/" . "|" .
         "hoge123@google.com" . "|" .
         "192.168.1.100" . "|" .
+        $i . "|" .
         "0\n", 3, $list
     );
 //    $temp2 = str_replace("<br>", "\n", $temp[6]);
     $temp2 = str_replace("<br>", "\n", get_sample_text($temp[0]));
     error_log($temp2, 3, $txt);
-//    $i++;
+    $i++;
 }
 
 $remade = file("list.txt");
+$remade = sort_list($remade);
 
 function get_sample_text($id){
     $array = [];
@@ -50,6 +52,64 @@ function get_sample_text($id){
         array_push($array, $id . " 番目の投稿です。");
     }
     return implode($array);
+}
+
+function get_id_array($list){
+    $array = [];
+    foreach ($list as $line){
+        $temp = explode("|", $line);
+        array_push($array, (int)$temp[0]);
+    }
+//    return sort($array);
+    sort($array);
+    return $array;
+}
+
+function sort_list($list){
+//    $lines = file($list);
+    $nums = get_id_array($list);
+    $sorted = [];
+//    $i = 0;
+    foreach ($nums as $num){
+        $temp2 = get_line($list, $num);
+//        array_push($sorted, $list[$num]);
+        array_push($sorted, $temp2);
+        error_log($temp2, 3, "list2.txt");
+    }
+//    foreach ($list as $line){
+////        $temp = explode("|", $line);
+////        $temp = get_line($lines, $i + 1);
+////        if((int)$temp[0] === (int)$temp[9]){
+////            array_push($sorted, $temp);
+////        }
+//        $temp = get_line($list, $i + 1);
+//        array_push($sorted, $temp);
+////        if($temp === null) {
+////            array_push($sorted, $line);
+////        } else {
+////            array_push($sorted, $temp);
+////        }
+////        error_log($temp, 3, "list2.txt");
+//        $i++;
+//    }
+    return $sorted;
+}
+
+function get_line($list, $id){
+//    $num = 64; // IDナンバーの最大値
+    foreach ($list as $line){
+        $temp = explode("|", $line);
+        if($id === (int)$temp[0]){
+            return $line;
+        }
+    }
+//    for($i = 0; $i < $num; $i++){
+//        $temp = explode("|", $list[$i]);
+//        if($id === (int)$temp[0]){
+//            return $list[$i];
+//        }
+//    }
+    return null;
 }
 
 //show_list();
